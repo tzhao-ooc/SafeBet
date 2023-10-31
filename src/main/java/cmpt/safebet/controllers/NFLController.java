fc17f9e5079675866adff03e2cc68d57b42ed8fe
Dilpreet Grewal <grewal.dilpreet15@gmail.com>
2023-10-30 18:28:24 -0700

    Added scores to gamestoday.html + css file

--- src/main/java/cmpt/safebet/controllers/NFLController.java
+++ src/main/java/cmpt/safebet/controllers/NFLController.java
@@ -8,14 +8,23 @@ import org.springframework.web.client.RestTemplate;
 public class NFLController {
 
     private static final String API_KEY = "78b1b4c59cc16d1112c63912aecc3e46";
-    private static final String BASE_URL = "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/";
+    private static final String ODDS_URL = "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/";
+    private static final String SCORES_URL = "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/scores/";
 
     @GetMapping("/nflodds")
     public String getNFLOdds() {
-        String url = BASE_URL + "?apiKey=" + API_KEY + "&regions=us&markets=h2h,spreads&oddsFormat=decimal";
+        String url = ODDS_URL + "?apiKey=" + API_KEY + "&regions=us&markets=h2h,spreads&oddsFormat=decimal";
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(url, String.class);
         return result; 
+    }
+
+    @GetMapping("/nflscores")
+    public String getNFLScores() {
+        String url = SCORES_URL + "?apiKey=" + API_KEY;
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(url, String.class);
+        return result;
     }
 }
 
