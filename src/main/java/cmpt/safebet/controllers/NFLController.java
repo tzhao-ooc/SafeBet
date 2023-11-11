30735c93f537f777ba132d43a065df2c46dd9672
Dilpreet Grewal <grewal.dilpreet15@gmail.com>
2023-11-10 16:47:20 -0800

    game scores for 3 days and fixed game status issue

--- src/main/java/cmpt/safebet/controllers/NFLController.java
+++ src/main/java/cmpt/safebet/controllers/NFLController.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class NFLController {
 
-    private static final String API_KEY = "78b1b4c59cc16d1112c63912aecc3e46";
+    private static final String API_KEY = "a7068f051dbed5d926f5f0d981da297f";
     private static final String ODDS_URL = "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/";
     private static final String SCORES_URL = "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/scores/";
 
@@ -21,7 +21,8 @@ public class NFLController {
 
     @GetMapping("/nflscores")
     public String getNFLScores() {
-        String url = SCORES_URL + "?apiKey=" + API_KEY;
+        // added scores from 3 previous days 
+        String url = SCORES_URL + "?apiKey=" + API_KEY + "&daysFrom=3";
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(url, String.class);
         return result;
