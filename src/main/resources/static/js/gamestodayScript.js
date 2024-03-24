9e10d8d87ba526ace4a3b2389d8d112f110ba05b
David Krljanovic <104551295+Eggstrovert@users.noreply.github.com>
2024-03-23 22:22:46 -0700

    Delete src directory

--- src/main/resources/static/js/gamestodayScript.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementsByName("betAmount")[0].addEventListener('change', checkAmount)
-    document.getElementsByName("")
-
-});
-
